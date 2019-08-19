db776896185b9cbe3583008fb5b4155adf57057c
Maarten Van der Hoef <ext.maarten.vanderhoef@flaconi.de>
2019-08-19 12:47:13 +0200

    FMT

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "sns_topic_subscriptions" {
   type = list(object({
-    name           = string
-    topic_arn               = string
+    name                   = string
+    topic_arn              = string
     protocol               = string
     endpoint               = string
-    endpoint_auto_confirms   = bool
+    endpoint_auto_confirms = bool
   }))
   default     = []
   description = "SNS Subscriptions"
