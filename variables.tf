48e697c49dd77967293525b230fea6292a69dfc0
Alexander Miehe <alexander.miehe@flaconi.de>
2020-01-02 12:56:27 +0100

    add missing raw_message_delivery typ

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "sns_topic_subscriptions" {
     protocol               = string
     endpoint               = string
     endpoint_auto_confirms = bool
+    raw_message_delivery   = bool
   }))
   default     = []
   description = "SNS Subscriptions"
